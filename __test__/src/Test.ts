9d0314b815e70b6b8d59abbcd8444934a2a148c9
Tolfx <matteus12122@gmail.com>
2021-06-19 13:06:02 +0200

    Modified __test__

--- __test__/src/Test.ts
+++ __test__/src/Test.ts
@@ -1,7 +1,13 @@
 import Webhook from "../../src/Main";
+import express from "express";
 
 const webhook = new Webhook(3000);
 
 webhook.listen("package", (body) => {
     console.log(body);
-})
+});
+
+const app = express();
+
+// Add support for express use?
+// app.use();
